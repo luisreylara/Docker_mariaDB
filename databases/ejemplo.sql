c697b1afb9b549cd705279bfb8badf44474eaa78
Luis Rey L <luisreylara@hotmail.com>
2025-06-23 13:54:51 -0600

    primer

---
+++ databases/ejemplo.sql
@@ -0,0 +1,5 @@
+insert into materias 
+values (100,'fisica');
+
+insert into materias 
+values (0,'electronica digital');
---
+++ databases/sql.sql
@@ -0,0 +1,14 @@
+
+
+
+
+
+use universidad;
+create table materias
+ (id int, nombre varchar(50));
+
+insert into materias 
+values (10,'poo');
+
+insert into materias 
+values (20,'sistemas informaticos distribuidos');
